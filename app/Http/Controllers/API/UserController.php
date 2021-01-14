4520691f872810fe6f9288cab222d6526f203852
montohang <harmonsitohang17@gmail.com>
2021-01-14 13:05:47 +0700

    [REBUILD PACKAGE]

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Actions\Fortify\PasswordValidationRules;
+use App\Helper\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    use PasswordValidationRules;
+
+    public function login(Request $request)
+    {
+        try{
+            // validasi input
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'password|required'
+            ]);
+
+            // cek credentials (login)
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Autthentication Failde', 500);
+            }
+
+            // jika hash tidak sesuai maka error
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            // jika berhasil maka sukses masuk
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+        } catch(Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failde', 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'max:255', 'unique:users'],
+                'password' => $this->passwordRules()
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
